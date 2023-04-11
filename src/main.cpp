4d477e5720b79533c717e1f2114ccc51f2b452a6
João Gerliandro <joaogerliandro@gmail.com>
2023-04-10 22:19:32 -0300

    feature - simple test server

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Project Config !" << std::endl; 
+    try
+    {
+        boost::asio::io_service io_service;
+
+        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 4444));
+        std::cout << "[SERVER]: Awaiting connections ..." << std::endl;
+
+        for(;;)
+        {
+            tcp::socket socket(io_service);
+
+            acceptor.accept(socket);
+            std::cout << "[SERVER]: New connection established" << std::endl;
+
+            std::string message = "Connected successfully !";
+
+            boost::system::error_code ignored_error;
+
+            boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+        }
+    }
+    catch(std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 
     return EXIT_SUCCESS;
 }
