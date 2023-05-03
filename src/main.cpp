7eb2489962dcd526ebe58bef18db6d2f4965be7b
João Gerliandro <joaogerliandro@gmail.com>
2023-05-02 22:37:20 -0300

    feature - adding a multi connection system using threads
    Co-Authored-By: Lucas <109928093+lucassdmp@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,24 @@
 
 using boost::asio::ip::tcp;
 
+void new_session(tcp::socket socket)
+{
+    for (;;)
+    {
+        boost::asio::streambuf response_buffer;
+
+        boost::asio::read_until(socket, response_buffer, "\n");
+
+        std::string response_message = boost::asio::buffer_cast<const char *>(response_buffer.data());
+
+        response_message.erase(std::remove(response_message.begin(), response_message.end(), '\n'), response_message.cend());
+
+        std::cout << "[" << socket.remote_endpoint() << "-RESPONSE]: " << response_message << std::endl;
+
+        boost::asio::write(socket, boost::asio::buffer("OK\n"));
+    }
+}
+
 int main(int argc, char *argv[])
 {
     try
@@ -15,7 +33,7 @@ int main(int argc, char *argv[])
         tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 4444));
         std::cout << "[SERVER]: Awaiting connections ..." << std::endl;
 
-        for(;;)
+        for (;;)
         {
             tcp::socket socket(io_service);
 
@@ -25,23 +43,10 @@ int main(int argc, char *argv[])
 
             boost::asio::write(socket, boost::asio::buffer("Welcome new connection !\n"));
 
-            for(;;)
-            {
-                boost::asio::streambuf response_buffer;
-
-                boost::asio::read_until(socket, response_buffer, "\n");
-
-                std::string response_message = boost::asio::buffer_cast<const char*>(response_buffer.data());
-
-                response_message.erase(std::remove(response_message.begin(), response_message.end(), '\n'), response_message.cend());
-
-                std::cout << "[" << socket.remote_endpoint() << "-RESPONSE]: " << response_message <<std::endl;
-
-                boost::asio::write(socket, boost::asio::buffer("OK\n"));
-            }
+            std::thread(new_session, std::move(socket)).detach();
         }
     }
-    catch(std::exception& e)
+    catch (std::exception &e)
     {
         std::cerr << e.what() << std::endl;
     }
