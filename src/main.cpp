edf14dc5963b76c55aa1a9c1f51d8205658d590d
João Gerliandro <joaogerliandro@gmail.com>
2023-06-16 13:46:43 -0300

    refactor - removing unnecessary messages

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ void new_session(tcp::socket socket)
 
         response_message.erase(std::remove(response_message.begin(), response_message.end(), '\n'), response_message.cend());
 
-        std::cout << "[" << socket.remote_endpoint() << "-RESPONSE]: " << response_message << std::endl;
+        std::cout << "[" << socket.remote_endpoint() << "]: " << response_message << std::endl;
 
         boost::asio::ip::port_type sender_port = socket.remote_endpoint().port();
         boost::asio::ip::address sender_adress = socket.remote_endpoint().address();
@@ -46,7 +46,7 @@ void new_session(tcp::socket socket)
                 sender_ip = "localhost";
             }
 
-            std::string global_mensage = "[" + sender_ip + "-RESPONSE]: " + response_message;
+            std::string global_mensage = "[" + sender_ip + "]: " + response_message;
             
             boost::asio::write(client.m_socket, boost::asio::buffer(global_mensage + "\n"));
         }
@@ -70,7 +70,7 @@ int main(int argc, char *argv[])
 
             std::cout << "[SERVER]: Connection established with " << socket.remote_endpoint() << std::endl;
 
-            boost::asio::write(socket, boost::asio::buffer("[SERVER-RESPONSE]: Welcome " + socket.remote_endpoint().address().to_string() + ":" + std::to_string(socket.remote_endpoint().port()) + " !\n"));
+            boost::asio::write(socket, boost::asio::buffer("[SERVER]: Welcome " + socket.remote_endpoint().address().to_string() + ":" + std::to_string(socket.remote_endpoint().port()) + " !\n"));
 
             std::thread(new_session, std::move(socket)).detach();
         }
