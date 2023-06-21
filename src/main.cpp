f8d8ff5e237e7e89195eda8332cb9e4634de69c4
João Gerliandro <joaogerliandro@gmail.com>
2023-06-21 09:39:51 -0300

    feature - adding a simple chat room system

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,26 @@
 
 using boost::asio::ip::tcp;
 
+std::uint32_t room_count = 0;
+
 class Client
 {
-    public:
-        Client(tcp::socket& socket) : m_socket(socket) {}
+public:
+    Client(tcp::socket &socket) : m_socket(socket) {}
 
-        tcp::socket& m_socket;
+    tcp::socket &m_socket;
+    std::uint32_t m_room_id;
 };
 
 class Room
 {
-    public:
-        Room(std::string room_name, std::uint8_t room_size, std::uint8_t room_id) : m_name(room_name), m_size(room_size), m_id(room_id) {}
-    
-        std::uint32_t m_id;
-        std::uint8_t m_size;
-        std::string m_name;
-        std::vector<Client> m_client_list;
+public:
+    Room(std::string room_name, std::uint8_t room_size) : m_name(room_name), m_size(room_size), m_id(++room_count) {}
+
+    std::uint32_t m_id;
+    std::uint8_t m_size;
+    std::string m_name;
+    std::vector<Client> m_client_list;
 };
 
 std::vector<Room> room_list;
@@ -33,18 +36,16 @@ std::string list_open_rooms()
 {
     std::string room_list_str;
 
-    for(Room room : room_list)
+    for (Room room : room_list)
         room_list_str += "\n- [" + room.m_name + "] ID:" + std::to_string(room.m_id) + " is open with " + std::to_string(room.m_size) + " spaces !";
 
     return room_list_str;
 }
 
-void connection_handshake(tcp::socket& socket)
+void connection_handshake(tcp::socket &socket)
 {
     Client new_client(socket);
-    
-    client_list.push_back(new_client);
-    
+
     std::string handshake_message = "[SERVER]: Welcome " + socket.remote_endpoint().address().to_string() + ":" + std::to_string(socket.remote_endpoint().port()) + " !";
 
     handshake_message += "\n[SERVER]: Open rooms in the Server: " + list_open_rooms();
@@ -53,34 +54,67 @@ void connection_handshake(tcp::socket& socket)
 
     bool handshake_is_over = false;
 
-    while(true)
+    while (true)
     {
-        boost::asio::streambuf response_buffer;
+        boost::asio::streambuf handshake_buffer;
 
-        boost::asio::read_until(socket, response_buffer, "\n");
+        boost::asio::read_until(socket, handshake_buffer, "\n");
 
-        std::string response_message = boost::asio::buffer_cast<const char *>(response_buffer.data());
+        handshake_message = boost::asio::buffer_cast<const char *>(handshake_buffer.data());
 
-        response_message.erase(std::remove(response_message.begin(), response_message.end(), '\n'), response_message.cend());
+        handshake_message.erase(std::remove(handshake_message.begin(), handshake_message.end(), '\n'), handshake_message.cend());
 
-        for(Room room : room_list)
+        for (Room room : room_list)
         {
-            if(std::stoul(response_message) == room.m_id)
+            if (std::stoul(handshake_message) == room.m_id)
             {
-                room.m_client_list.push_back(new_client);
+                new_client.m_room_id = room.m_id;
 
                 boost::asio::write(socket, boost::asio::buffer("[SERVER]: You have connected to " + room.m_name + "!\n"));
 
+                client_list.push_back(new_client);
+                room_list[room.m_id - 1].m_client_list.push_back(new_client);
+
                 handshake_is_over = true;
                 break;
-            }       
+            }
         }
 
-        if(handshake_is_over)
+        if (handshake_is_over)
             break;
 
         boost::asio::write(socket, boost::asio::buffer("NOK\n"));
     }
+}
+
+std::string listen_client(tcp::socket& socket)
+{
+    boost::asio::streambuf response_buffer;
+
+    boost::asio::read_until(socket, response_buffer, "\n");
+
+    std::string response_message = boost::asio::buffer_cast<const char *>(response_buffer.data());
+
+    response_message.erase(std::remove(response_message.begin(), response_message.end(), '\n'), response_message.cend());
+
+    return response_message;
+}
+
+void forward_message(Client &sender_client, std::string response_message)
+{
+    boost::asio::ip::tcp::endpoint sender_endpoint = sender_client.m_socket.remote_endpoint();
+
+    std::string sender_ip = sender_endpoint.address().to_string() + ":" + std::to_string(sender_endpoint.port());
+
+    Room receiver_room = room_list[sender_client.m_room_id - 1];
+
+    std::cout << "[" << receiver_room.m_name << "]" << "-[" << sender_endpoint << "]: " << response_message << std::endl;
+
+    for (Client receiver_client : receiver_room.m_client_list)
+        if (receiver_client.m_socket.remote_endpoint() == sender_endpoint)
+            boost::asio::write(receiver_client.m_socket, boost::asio::buffer("[LOCALHOST]: " + response_message + "\n"));
+        else
+            boost::asio::write(receiver_client.m_socket, boost::asio::buffer("[" + sender_ip + "]: " + response_message + "\n"));
 }
 
 void new_session(tcp::socket socket)
@@ -89,32 +123,11 @@ void new_session(tcp::socket socket)
 
     while (true)
     {
-        boost::asio::streambuf response_buffer;
+        std::string response_message = listen_client(socket);
 
-        boost::asio::read_until(socket, response_buffer, "\n");
-
-        std::string response_message = boost::asio::buffer_cast<const char *>(response_buffer.data());
-
-        response_message.erase(std::remove(response_message.begin(), response_message.end(), '\n'), response_message.cend());
-
-        std::cout << "[" << socket.remote_endpoint() << "]: " << response_message << std::endl;
-
-        boost::asio::ip::tcp::endpoint sender_endpoint = socket.remote_endpoint();
-
-        std::string sender_ip = sender_endpoint.address().to_string() + ":" + std::to_string(sender_endpoint.port());
-
-        for(Client client : client_list)
-        {
-            std::string global_mensage;
-
-            if(client.m_socket.remote_endpoint().address() == sender_endpoint.address() &&
-               client.m_socket.remote_endpoint().port() == sender_endpoint.port())
-                global_mensage = "[LOCALHOST]: " + response_message;
-            else
-                global_mensage = "[" + sender_ip + "]: " + response_message;
-            
-            boost::asio::write(client.m_socket, boost::asio::buffer(global_mensage + "\n"));
-        }
+        for (Client client : client_list)
+            if (client.m_socket.remote_endpoint() == socket.remote_endpoint())
+                forward_message(client, response_message);
     }
 }
 
@@ -122,8 +135,8 @@ int main(int argc, char *argv[])
 {
     try
     {
-        room_list.push_back(Room(std::string("DEFAULT-ROOM"), 8, 1));
-        room_list.push_back(Room(std::string("PRIVATE-ROOM"), 2, 2));
+        room_list.push_back(Room(std::string("DEFAULT-ROOM"), 8));
+        room_list.push_back(Room(std::string("PRIVATE-ROOM"), 2));
 
         boost::asio::io_service io_service;
 
