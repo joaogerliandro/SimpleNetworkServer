225d0185f577bcb5acadd26497f87273c0e17eb1
João Gerliandro <joaogerliandro@gmail.com>
2023-10-13 14:57:51 -0300

    feature - adding a new method of handle the client response

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ public:
         return *this;
     }
 
-    bool operator==(const Client& other) const
+    bool operator==(const Client &other) const
     {
         return (m_id == other.m_id && m_endpoint == other.m_endpoint);
     }
@@ -51,18 +51,27 @@ public:
     std::vector<Client> m_client_list;
 };
 
+enum REQUESTED_METHOD
+{
+    UNMAPED_METHOD = 0,
+    LIST_OPEN_ROOMS = 1,
+    FOWARD_MESSAGE = 2,
+    CONNECT_TO_ROOM = 3,
+    EXIT_FROM_ROOM = 4
+};
+
+class Response
+{
+public:
+    Client *client;
+
+    std::string message;
+
+    REQUESTED_METHOD requested_method;
+};
+
 std::vector<Room> room_list;
 std::vector<Client> client_list;
-
-std::string list_open_rooms()
-{
-    std::string room_list_str;
-
-    for (Room room : room_list)
-        room_list_str += "ID:" + std::to_string(room.m_id) + ";Name:" + room.m_name + ";Size:" + std::to_string(room.m_size) + ";Connected:" + std::to_string(room.m_client_list.size()) + "\n";
-
-    return room_list_str;
-}
 
 void forward_welcome_message(Client &sender_client, Room &receiver_room)
 {
@@ -79,63 +88,17 @@ void connection_handshake(tcp::socket *socket)
 {
     Client new_client(socket);
 
-    boost::asio::streambuf user_buffer; 
+    boost::asio::streambuf user_buffer;
     boost::asio::read_until(*socket, user_buffer, '\n');
 
     std::string user_name = boost::asio::buffer_cast<const char *>(user_buffer.data());
-    
+
     user_name.erase(std::remove(user_name.begin(), user_name.end(), '\n'), user_name.cend());
 
-    boost::asio::write(*socket, boost::asio::buffer(list_open_rooms() + "\n"));
+    new_client.m_id = client_count++;
+    new_client.m_name = user_name;
 
-    bool handshake_is_over = false;
-
-    while (true)
-    {
-        boost::asio::streambuf handshake_buffer;
-
-        boost::asio::read_until(*socket, handshake_buffer, "\n");
-
-        std::string handshake_message = boost::asio::buffer_cast<const char *>(handshake_buffer.data());
-
-        handshake_message.erase(std::remove(handshake_message.begin(), handshake_message.end(), '\n'), handshake_message.cend());
-
-        for (Room room : room_list)
-        {
-            if (std::stoul(handshake_message) == room.m_id)
-            {
-                new_client.m_id = client_count++;
-                new_client.m_name = user_name;
-                new_client.m_room_id = room.m_id;
-
-                std::cout << "[SERVER]: Client [" << socket->remote_endpoint() << "]-[" << user_name << "] have connected to [" << room.m_name << "]" << std::endl;
-
-                client_list.push_back(new_client);
-                room_list[room.m_id - 1].m_client_list.push_back(new_client);
-
-                forward_welcome_message(new_client, room_list[room.m_id - 1]);
-
-                handshake_is_over = true;
-                break;
-            }
-        }
-
-        if (handshake_is_over)
-            break;
-    }
-}
-
-std::string listen_client(tcp::socket *socket)
-{
-    boost::asio::streambuf response_buffer;
-
-    boost::asio::read_until(*socket, response_buffer, "\n");
-
-    std::string response_message = boost::asio::buffer_cast<const char *>(response_buffer.data());
-
-    response_message.erase(std::remove(response_message.begin(), response_message.end(), '\n'), response_message.cend());
-
-    return response_message;
+    client_list.push_back(new_client);
 }
 
 void remove_client(Client &client)
@@ -145,13 +108,13 @@ void remove_client(Client &client)
     bool client_found = false;
     std::vector<Client>::iterator room_client_it = room_list[client.m_room_id - 1].m_client_list.begin();
 
-    for(Client other_client : room_list[client.m_room_id - 1].m_client_list)
+    for (Client other_client : room_list[client.m_room_id - 1].m_client_list)
     {
-        if(other_client.m_id != client.m_id)
+        if (other_client.m_id != client.m_id)
         {
             boost::asio::write(*(other_client.m_socket), boost::asio::buffer("[<font color='green'>SERVER</font>]: Client [" + client.m_name + "] have disconnected !\n"));
-            
-            if(!client_found)
+
+            if (!client_found)
             {
                 room_client_it++;
             }
@@ -168,9 +131,9 @@ void remove_client(Client &client)
 
     std::vector<Client>::iterator client_it = client_list.begin();
 
-    for(Client other_client : client_list)
+    for (Client other_client : client_list)
     {
-        if(other_client.m_id != client.m_id)
+        if (other_client.m_id != client.m_id)
         {
             client_it++;
         }
@@ -178,26 +141,127 @@ void remove_client(Client &client)
         {
             break;
         }
-    }   
+    }
 
     client_list.erase(client_it);
 }
 
-void forward_message(Client &sender_client, std::string response_message)
+void connect_to_room(Client& sender_client, std::string response_message)
 {
-    boost::asio::ip::tcp::endpoint sender_endpoint = sender_client.m_endpoint;
+    uint32_t room_id = std::stoul(response_message);
 
-    Room receiver_room = room_list[sender_client.m_room_id - 1];
+    for (Room room : room_list)
+    {
+        if (room_id == room.m_id)
+        {
+            sender_client.m_room_id = room.m_id;
+
+            std::cout << "[SERVER]: Client [" << sender_client.m_socket->remote_endpoint() << "]-[" << sender_client.m_name << "] have connected to [" << room.m_name << "]" << std::endl;
+
+            room_list[room.m_id - 1].m_client_list.push_back(sender_client);
+
+            forward_welcome_message(sender_client, room_list[room.m_id - 1]);
+        }
+    }
+}
+
+void exit_from_room(Client *sender_client)
+{
+}
+
+void forward_message(Client *sender_client, std::string response_message)
+{
+    boost::asio::ip::tcp::endpoint sender_endpoint = sender_client->m_endpoint;
+
+    Room receiver_room = room_list[sender_client->m_room_id - 1];
 
     std::cout << "[" << receiver_room.m_name << "]"
               << "-[" << sender_endpoint << "]"
-              << "-[" << sender_client.m_name << "]: " << response_message << std::endl;
+              << "-[" << sender_client->m_name << "]: " << response_message << std::endl;
 
     for (Client receiver_client : receiver_room.m_client_list)
         if (receiver_client.m_endpoint == sender_endpoint)
             boost::asio::write(*receiver_client.m_socket, boost::asio::buffer("[<font color='blue'>" + receiver_client.m_name + "</font>]: " + response_message + "\n"));
         else
-            boost::asio::write(*receiver_client.m_socket, boost::asio::buffer("[<font color='red'>" + sender_client.m_name + "</font>]: " + response_message + "\n"));
+            boost::asio::write(*receiver_client.m_socket, boost::asio::buffer("[<font color='red'>" + sender_client->m_name + "</font>]: " + response_message + "\n"));
+}
+
+void list_open_rooms(tcp::socket *socket)
+{
+    std::string room_list_str;
+
+    for (Room room : room_list)
+        room_list_str += "ID:" + std::to_string(room.m_id) + ";Name:" + room.m_name + ";Size:" + std::to_string(room.m_size) + ";Connected:" + std::to_string(room.m_client_list.size()) + "\n";
+
+    boost::asio::write(*socket, boost::asio::buffer(room_list_str + "\n"));
+}
+
+void response_handler(Response response)
+{
+    switch (response.requested_method)
+    {
+        case REQUESTED_METHOD::LIST_OPEN_ROOMS:
+            list_open_rooms(response.client->m_socket);
+            break;
+        case REQUESTED_METHOD::FOWARD_MESSAGE:
+            forward_message(response.client, response.message);
+            break;
+        case REQUESTED_METHOD::CONNECT_TO_ROOM:
+            connect_to_room(*response.client, response.message);
+            break;
+        case REQUESTED_METHOD::EXIT_FROM_ROOM:
+            exit_from_room(response.client);
+            break;
+    }
+}
+
+REQUESTED_METHOD map_requested_method(std::string requested_method)
+{
+    if(requested_method == "LIST_OPEN_ROOMS")
+        return REQUESTED_METHOD::LIST_OPEN_ROOMS;
+    else if(requested_method == "FOWARD_MESSAGE")
+        return REQUESTED_METHOD::FOWARD_MESSAGE;
+    else if(requested_method == "CONNECT_TO_ROOM")
+        return REQUESTED_METHOD::CONNECT_TO_ROOM;
+    else if(requested_method == "EXIT_FROM_ROOM")
+        return REQUESTED_METHOD::EXIT_FROM_ROOM;
+    else
+        return REQUESTED_METHOD::UNMAPED_METHOD;
+}
+
+void parse_response(Response *response, std::string message)
+{
+    std::stringstream str_stream(message);
+
+    std::string requested_method;
+    std::string response_message;
+
+    str_stream >> requested_method;
+    str_stream >> response_message;
+
+    response->requested_method = map_requested_method(requested_method);
+    response->message = response_message;
+}
+
+Response listen_client(tcp::socket *socket)
+{
+    Response socket_response;
+
+    boost::asio::streambuf response_buffer;
+
+    boost::asio::read_until(*socket, response_buffer, "\n");
+
+    std::string response_message = boost::asio::buffer_cast<const char *>(response_buffer.data());
+
+    response_message.erase(std::remove(response_message.begin(), response_message.end(), '\n'), response_message.cend());
+
+    for (Client client : client_list)
+        if (client.m_socket == socket)
+            socket_response.client = &client;
+
+    parse_response(&socket_response, response_message);
+
+    return socket_response;
 }
 
 void new_session(tcp::socket *socket)
@@ -210,11 +274,9 @@ void new_session(tcp::socket *socket)
 
         while (true)
         {
-            std::string response_message = listen_client(socket);
+            Response client_response = listen_client(socket);
 
-            for (Client client : client_list)
-                if (client.m_socket == socket)
-                    forward_message(client, response_message);
+            response_handler(client_response);
         }
     }
     catch (const boost::system::system_error &system_error)
